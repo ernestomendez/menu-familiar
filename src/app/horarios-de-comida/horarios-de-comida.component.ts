6ea0cba823c3141984b53d0479859ddbba2bfc06
Ernesto Mendez <ernesto.mendez@ihg.com>
2018-12-12 14:21:53 -0600

    created horarios de comida component

---
+++ src/app/horarios-de-comida/horarios-de-comida.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-horarios-de-comida',
+  templateUrl: './horarios-de-comida.component.html',
+  styleUrls: ['./horarios-de-comida.component.css']
+})
+export class HorariosDeComidaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
